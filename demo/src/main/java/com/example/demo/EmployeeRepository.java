b6162ee22ef0c0f29a226e35e54fa995a3dfa732
Matheus Afonso <84944531+MatAfonso@users.noreply.github.com>
2024-08-30 01:34:38 +0000

    Criação de API

--- demo/src/main/java/com/example/demo/EmployeeRepository.java
+++ demo/src/main/java/com/example/demo/EmployeeRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-
 }
